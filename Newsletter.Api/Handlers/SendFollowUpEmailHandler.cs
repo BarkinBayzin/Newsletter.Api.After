8cb0fbc36fdaa925019c3c52a0be9972f3f8ca34
Barkın Bayzin <barkin.bayzin@negzel.net>
2024-09-25 16:48:22 +0300

    Add project files.

---
+++ Newsletter.Api/Handlers/SendFollowUpEmailHandler.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Newsletter.Api.Emails;
+using Newsletter.Api.Messages;
+
+namespace Newsletter.Api.Handlers;
+
+public class SendFollowUpEmailHandler(IEmailService emailService) : IConsumer<SendFollowUpEmail>
+{
+    public async Task Consume(ConsumeContext<SendFollowUpEmail> context)
+    {
+        await emailService.SendFollowUpEmailAsync(context.Message.Email);
+
+        await context.Publish(new FollowUpEmailSent
+        {
+            SubscriberId = context.Message.SubscriberId,
+            Email = context.Message.Email
+        });
+    }
+}
